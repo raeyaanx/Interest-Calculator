e1c540b79e68f88d16171b1b8b037ee8d8194819
Raeyaan Arshad <154695298+raeyaanx@users.noreply.github.com>
2024-12-26 21:07:41 +0530

    Initial C++ code

---
+++ interest.cpp
@@ -0,0 +1,19 @@
+//Program to calculate simple interest and compound interest
+//si = simple interest | ci = compound interest
+
+/* DEV STAGE 1 */
+
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+
+// SIMPLE INTEREST FUNCTION
+
+
+// COMPOUND INTEREST FUNCTION
+
+
+int main()
+{
+    std::cout << "Program to calculate Simple Interest & Compound Interest" << "\n";
+}
